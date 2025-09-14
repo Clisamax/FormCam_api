157919d49e71659ddefeb1862ce1b177e06eec98
clisamax <clisamax.gomes.dev@gmail.com>
2025-09-14 18:29:37 -0300

    refactor(routes): restructure route files and remove @ prefix
    Standardize route file naming by removing @ prefix and update imports in main routes file. Add new product routes and maintain existing functionality.

--- src/routes/@routes.ts
+++ src/routes/@routes.ts
@@ -1,8 +1,10 @@
 import { fast } from "../server"
-import { routesOccurrences } from "./@routesOccurrence"
-import { routesUsers } from "./@routes_users"
+import { routesOccurrences } from "./routesOccurrence"
+import { routesProducts } from "./routes_product"
+import { routesUsers } from "./routes_users"
 
 export const Routes = async () => {
 	fast.register(routesUsers)
 	fast.register(routesOccurrences)
+	fast.register(routesProducts)
 }
---
+++ src/routes/routes_product.ts
@@ -0,0 +1,6 @@
+import { ProductRepository } from "../modules/product/repositories/product.repository"
+import { fast } from "../server"
+
+export const routesProducts = async () => {
+	fast.register(routesProducts)
+}
